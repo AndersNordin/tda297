2f7ac7cf553b3c2ad7d4cdd1b685ef133b625276
ViktorLindstrm <viktor.lindstrm@gmail.com>
2014-02-10 18:16:31 +0100

    Fixat sekvensnummer och så att meddelande ackas

--- Lab2/ExampleCaster.java
+++ Lab2/ExampleCaster.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 import mcgui.*;
 
 /**
@@ -11,22 +11,28 @@ public class ExampleCaster extends Multicaster {
     /**
      * No initializations needed for this simple one
      */
+    private static long seqNr;
+    private static ArrayList<ExampleMessage> history = null;
     public void init() {
+	history = new ArrayList<ExampleMessage>();
         mcui.debug("The network has "+hosts+" hosts!");
+	seqNr = 0;
     }
         
     /**
      * The GUI calls this module to multicast a message
      */
     public void cast(String messagetext) {
+	long seqNumber = ++seqNr;
         for(int i=0; i < hosts; i++) {
             /* Sends to everyone except itself */
             if(i != id) {
-                bcom.basicsend(i,new ExampleMessage(id, messagetext));
+                bcom.basicsend(i,new ExampleMessage(id, messagetext,seqNumber));
             }
         }
-        mcui.debug("Sent out: \""+messagetext+"\"");
+        mcui.debug("Sent message: \""+messagetext+"\" seq: "+seqNr);
         mcui.deliver(id, messagetext, "from myself!");
+	history.add(new ExampleMessage(id,messagetext,seqNumber));
     }
     
     /**
@@ -34,7 +40,22 @@ public class ExampleCaster extends Multicaster {
      * @param message  The message received
      */
     public void basicreceive(int peer,Message message) {
-        mcui.deliver(peer, ((ExampleMessage)message).text);
+	ExampleMessage msg = (ExampleMessage)message;
+	if(msg.getSeqNr()>seqNr) {
+	    seqNr = msg.getSeqNr();
+	}
+	/**
+	 * if ack, then acknowledge this, else, treat it as a normal message
+	 * and answer with ack
+	 */
+	mcui.debug("Got message, is ack: "+msg.isAck());
+	if(msg.isAck()) {
+	    mcui.debug("Ack: "+msg.getSeqNr()+ " from: "+msg.getSenderId());
+	}else {
+	    bcom.basicsend(msg.getSenderId(),new ExampleMessage(id,true,seqNr));
+	    mcui.deliver(peer, msg.getText());    
+	}
+        		
     }
 
     /**
--- Lab2/ExampleMessage.java
+++ Lab2/ExampleMessage.java
@@ -8,11 +8,27 @@ import mcgui.*;
  */
 public class ExampleMessage extends Message {
         
-    String text;
-        
-    public ExampleMessage(int sender,String text) {
+    private String text;  
+    private long seqNr;
+    private boolean ack;
+    private int senderId;
+    /**
+     * constructor for sending a ack.
+     */
+    public ExampleMessage(int sender, boolean ack,long seqNr) {
+	super(sender);
+	this.ack = ack;
+	this.senderId = sender;
+	this.seqNr = seqNr;
+	text ="";
+    }
+
+    public ExampleMessage(int sender,String text,long seqNr) {
         super(sender);
+	senderId=sender;
         this.text = text;
+	this.seqNr = seqNr;
+	ack = false;
     }
     
     /**
@@ -23,6 +39,14 @@ public class ExampleMessage extends Message {
     public String getText() {
         return text;
     }
-    
+    public long getSeqNr() { 
+	return seqNr;
+    }
+    public boolean isAck(){
+	return ack;
+    }
+    public int getSenderId(){
+	return senderId;
+    }
     public static final long serialVersionUID = 0;
 }
